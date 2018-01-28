87a433775b1cd40347a5698eee2fcd68ee0cab93
Holayn <kwwong1597@gmail.com>
2018-01-27 19:53:37 -0500

    Created an enumeration for tokens. Wrote regular expressions for left and right brace. Still experimenting with code architecture

--- dist/scripts/lexer.js
+++ dist/scripts/lexer.js
@@ -1,7 +1,7 @@
 /* lexer.ts  */
 var TSC;
 (function (TSC) {
-    var Lexer = (function () {
+    var Lexer = /** @class */ (function () {
         function Lexer() {
         }
         Lexer.lex = function () {
@@ -10,11 +10,45 @@ var TSC;
                 var sourceCode = document.getElementById("taSourceCode").value;
                 // Trim the leading and trailing spaces.
                 sourceCode = TSC.Utils.trim(sourceCode);
+                console.log(sourceCode);
+                // We need to recognize different tokens.
+                // Thus, we need to have the different patterns for each token defined.
+                // A lexeme is a sequence of characters in the source that matches a pattern for a token.
+                // When a lexeme matches a pattern, create a new instance of that token.
+                // Take the input and create tokens
+                // Have a RegExp for each kind of token
+                // Define an enumeration for tokens
+                var Token = void 0;
+                (function (Token) {
+                    Token[Token["TLBrace"] = 0] = "TLBrace";
+                    Token[Token["TRBrace"] = 1] = "TRBrace";
+                })(Token || (Token = {}));
+                // Define array to return tokens in
+                var tokens_1 = [];
+                // Iterate through the input
+                var sourceCodePtr = 0;
+                while (sourceCodePtr < sourceCode.length) {
+                    // RegExp for Left Brace
+                    var rLBRACE = new RegExp('{');
+                    if (rLBRACE.test(sourceCode.charAt(sourceCodePtr))) {
+                        // Create a new TLBRACE token and add it to the array
+                        var token = Token.TLBrace;
+                        tokens_1.push(token);
+                    }
+                    // RegExp for Right Brace
+                    var rRBRACE = new RegExp('}');
+                    if (rRBRACE.test(sourceCode.charAt(sourceCodePtr))) {
+                        // Create a new TLBRACE token and add it to the array
+                        var token = Token.TRBrace;
+                        tokens_1.push(token);
+                    }
+                    sourceCodePtr++;
+                }
                 // TODO: remove all spaces in the middle; remove line breaks too.
                 return sourceCode;
             }
         };
         return Lexer;
-    })();
+    }());
     TSC.Lexer = Lexer;
 })(TSC || (TSC = {}));
--- source/scripts/lexer.ts
+++ source/scripts/lexer.ts
@@ -8,7 +8,55 @@ module TSC
 		        // Grab the "raw" source code.
 		        var sourceCode = (<HTMLInputElement>document.getElementById("taSourceCode")).value;
 		        // Trim the leading and trailing spaces.
-		        sourceCode = TSC.Utils.trim(sourceCode);
+				sourceCode = TSC.Utils.trim(sourceCode);
+				console.log(sourceCode);
+				// We need to recognize different tokens.
+				// Thus, we need to have the different patterns for each token defined.
+				// A lexeme is a sequence of characters in the source that matches a pattern for a token.
+				// When a lexeme matches a pattern, create a new instance of that token.
+				// Take the input and create tokens
+				// Have a RegExp for each kind of token
+				
+				// Define an enumeration for tokens
+				enum Token {
+					TLBrace,
+					TRBrace
+				}
+
+				// Define array to return tokens in
+				let tokens = [];
+
+				// Iterate through the input
+				let sourceCodePtr = 0;
+				while(sourceCodePtr < sourceCode.length){
+					// RegExp for Left Brace
+					let rLBRACE = new RegExp('{');
+					if(rLBRACE.test(sourceCode.charAt(sourceCodePtr))){
+						// Create a new TLBRACE token and add it to the array
+						var token: Token = Token.TLBrace
+						tokens.push(token);
+					}
+
+					// RegExp for Right Brace
+					let rRBRACE = new RegExp('}');
+					if(rRBRACE.test(sourceCode.charAt(sourceCodePtr))){
+						// Create a new TLBRACE token and add it to the array
+						var token: Token = Token.TRBrace
+						tokens.push(token);
+					}
+
+
+
+
+
+					sourceCodePtr++;
+				}
+				
+				
+				
+
+
+
 		        // TODO: remove all spaces in the middle; remove line breaks too.
 		        return sourceCode;
 		    }
