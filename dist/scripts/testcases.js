cc34642b78883e71fa727cc6f882fb420f491557
Holayn <kwwong15@punahou.edu>
2018-02-05 00:04:07 -0500

    added test cases to compiler

--- dist/scripts/testcases.js
+++ dist/scripts/testcases.js
@@ -9,12 +9,39 @@ var TSC;
         function Testcases() {
         }
         Testcases.load = function (testcase) {
-            switch (testcase) {
+            switch (testcase.innerHTML) {
                 case "Simple 1":
                     document.getElementById("taSourceCode").value = "/* This is a simple program with no operations */\n{}$";
                     break;
                 case "Simple 2":
-                    document.getElementById("taSourceCode").value = "/* This is a simple program with no operations */\n{}$";
+                    document.getElementById("taSourceCode").value = "/* Test case for print statement */\n/* Test case for print statement */\n{\n    print(\"i love compilers\")\n}$";
+                    break;
+                case "Regular":
+                    document.getElementById("taSourceCode").value = "/* Test case for a 'regular' program*/\n{\n    int a\n    a = 1\n    print(a)\n    boolean b\n    b = true\n    print(b)\n\n    {\n        int a\n        a = 2\n        print(a)\n    }\n\n    {\n        int a\n        a = 3\n        print(a)\n    }\n\n    string s\n    s = \"stra\"\n    print(s)\n\n    s = \"strb\"\n    print(s)\n\n    if (a != 5) {\n        print(\"true\")\n    }\n\n    if (a == 5) {\n        print(\"false\")\n    }\n}$";
+                    break;
+                case "Multiple":
+                    document.getElementById("taSourceCode").value = "/* Test case for multiple programs */\n{\n    print(\"i love compilers\")\n    int a\n    a = 2\n    string s\n    s = \"ha\"\n    \"\n}$\n\n{\n    int b\n    b = 4\n    string s\n    s = \"hey\"\n}$";
+                    break;
+                case "Crazy One Liner (Lex Pass)":
+                    document.getElementById("taSourceCode").value = "/* Test case for crazy one liner */\n+${hellotruefalsetrueprinta=3b=0print(\"false true\")whi33leiftruefalsestring!= stringintbooleanaa truewhileif{hi+++==!==}}/*aaahaha*/hahahahaha/*awao*/$";
+                    break;
+                case "Missing EOP":
+                    document.getElementById("taSourceCode").value = "/* Missing EOP */\n{\n    int b\n    b = 4\n    string s\n    s = \"hey\"\n}";
+                    break;
+                case "Alan":
+                    document.getElementById("taSourceCode").value = "/*  Provided By \n- Compiler Tyrant\n- Alan G Labouseur\n*/\n{}$\t\n{{{{{{}}}}}}$\t\n{{{{{{}}}}}}}$\t\n{int\t@}$";
+                    break;
+                case "Invalid String 1":
+                    document.getElementById("taSourceCode").value = "/* Test case for placing $ in quotes */\n{\n    print(\"i love com$pilers\")\n    int a\n    a = 2\n    string s\n    s = \"ha\"\n    \"\n}$";
+                    break;
+                case "Invalid String 2":
+                    document.getElementById("taSourceCode").value = "/* Test case for invalid characters in string */\n{\n    string s\n    s = \"cookies & cream\"\n}$";
+                    break;
+                case "Invalid String 3":
+                    document.getElementById("taSourceCode").value = "/* Test case for placing \\n in quotes */\n{\n    \"hey\n    there\"\n}$";
+                    break;
+                case "Invalid Print":
+                    document.getElementById("taSourceCode").value = "/* Test case for invalid print */\n{\n    print(\"my name is 11\")\n}$";
                     break;
             }
         };
--- source/scripts/testcases.ts
+++ source/scripts/testcases.ts
@@ -9,12 +9,125 @@
         export class Testcases {
     
         public static load(testcase: any){
-            switch(testcase){
+            switch(testcase.innerHTML){
                 case "Simple 1":
                     (<HTMLInputElement>document.getElementById("taSourceCode")).value = `/* This is a simple program with no operations */\n{}$`;
                     break;
                 case "Simple 2":
-                    (<HTMLInputElement>document.getElementById("taSourceCode")).value = `/* This is a simple program with no operations */\n{}$`;
+                    (<HTMLInputElement>document.getElementById("taSourceCode")).value = `/* Test case for print statement */
+/* Test case for print statement */
+{
+    print("i love compilers")
+}$`;
+                    break;
+                case "Regular":
+                    (<HTMLInputElement>document.getElementById("taSourceCode")).value = `/* Test case for a 'regular' program*/
+{
+    int a
+    a = 1
+    print(a)
+    boolean b
+    b = true
+    print(b)
+
+    {
+        int a
+        a = 2
+        print(a)
+    }
+
+    {
+        int a
+        a = 3
+        print(a)
+    }
+
+    string s
+    s = "stra"
+    print(s)
+
+    s = "strb"
+    print(s)
+
+    if (a != 5) {
+        print("true")
+    }
+
+    if (a == 5) {
+        print("false")
+    }
+}$`;
+                    break;
+                case "Multiple":
+                    (<HTMLInputElement>document.getElementById("taSourceCode")).value = `/* Test case for multiple programs */
+{
+    print("i love compilers")
+    int a
+    a = 2
+    string s
+    s = "ha"
+    "
+}$
+
+{
+    int b
+    b = 4
+    string s
+    s = "hey"
+}$`;
+                    break;
+                case "Crazy One Liner (Lex Pass)":
+                    (<HTMLInputElement>document.getElementById("taSourceCode")).value = `/* Test case for crazy one liner */\n+\${hellotruefalsetrueprinta=3b=0print(\"false true\")whi33leiftruefalsestring!= stringintbooleanaa truewhileif{hi+++==!==}}/*aaahaha*/hahahahaha/*awao*/$`;
+                    break;
+                case "Missing EOP":
+                    (<HTMLInputElement>document.getElementById("taSourceCode")).value = `/* Missing EOP */
+{
+    int b
+    b = 4
+    string s
+    s = "hey"
+}`;
+                    break;
+                case "Alan":
+                    (<HTMLInputElement>document.getElementById("taSourceCode")).value = `/*  Provided By 
+- Compiler Tyrant
+- Alan G Labouseur
+*/
+{}$	
+{{{{{{}}}}}}$	
+{{{{{{}}}}}}}$	
+{int	@}$`;
+                    break;
+                case "Invalid String 1":
+                    (<HTMLInputElement>document.getElementById("taSourceCode")).value = `/* Test case for placing $ in quotes */
+{
+    print("i love com$pilers")
+    int a
+    a = 2
+    string s
+    s = "ha"
+    "
+}$`;
+                    break;
+                case "Invalid String 2":
+                    (<HTMLInputElement>document.getElementById("taSourceCode")).value = `/* Test case for invalid characters in string */
+{
+    string s
+    s = "cookies & cream"
+}$`;
+                    break;
+                case "Invalid String 3":
+                    (<HTMLInputElement>document.getElementById("taSourceCode")).value = `/* Test case for placing \\n in quotes */
+{
+    "hey
+    there"
+}$`;
+                    break;
+                case "Invalid Print":
+                    (<HTMLInputElement>document.getElementById("taSourceCode")).value = `/* Test case for invalid print */
+{
+    print("my name is 11")
+}$`;
                     break;
             }
         }
